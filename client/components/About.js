8ab9831b78748d33829467e99028dae7b45b6664
Wesley Choi <weschoi.creative@gmail.com>
2018-10-23 18:11:29 -0700

    mobile view

--- client/components/About.js
+++ client/components/About.js
@@ -11,7 +11,10 @@ export default class About extends React.Component {
     return (
         <div className="section about">
             <div className="section-title">about</div>
-            <span>I like creating things that are useful beyond their visual appeal and meaningful enough to genuinely impact people’s lives. I also like user interfaces. And data presentation. Mostly developing experiences that are unique and complementary to natural human behavior. Here are some other things I like: football, basketball, stand-up comedy and podcasts.</span>
+
+            <div className="description">
+              <span>I like creating things that are useful beyond their visual appeal and meaningful enough to genuinely impact people’s lives. I also like user interfaces. And data presentation. Mostly developing experiences that are unique and complementary to natural human behavior. Here are some other things I like: football, basketball, stand-up comedy and podcasts.</span>
+            </div>
         </div>
     )
   }
