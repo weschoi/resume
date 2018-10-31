343786276378d212a5c585ef010a3c4f53004e1d
Wesley Choi <weschoi.creative@gmail.com>
2018-10-31 13:40:23 -0700

    edit mobile view

--- client/components/Experience.js
+++ client/components/Experience.js
@@ -12,21 +12,27 @@ export default class Experience extends React.Component {
         
           <div className="subsection">
             <span>2018&ensp;<span><a href="https://www.google.com/" style={{"textDecoration": "none", "color": "black"}}>google</a>&ensp;–&ensp;graphic designer [contract]</span></span>
-            <div>Developed single page website that reinvents user experience of journalistic content</div>
-            <div>Implemented magic scroll that dynamically changes interface with user scrolling</div>
-            <div>Built collapsible nav menu from scratch while staying true to original mockup</div>
+            <ul>
+              <li>Developed single page website that reinvents user experience of journalistic content</li>
+              <li>Implemented magic scroll that dynamically changes interface with user scrolling</li>
+              <li>Built collapsible nav menu from scratch while staying true to original mockup</li>
+            </ul>
           </div>
 
           <div className="subsection">
             <span>2017&ensp;<span><a href="https://www.box.com/" style={{"textDecoration": "none", "color": "black"}}>box</a>&ensp;–&ensp;presentation designer [contract]</span></span>
-            <div>Applied brand guidelines and messaging strategies to create over 20 slideshows for boxworks 2017</div>
-            <div>Worked with brand team to revamp customer pitch decks and all accompanying visuals; e.g. infographics</div>
+            <ul>
+              <li>Applied brand guidelines and messaging strategies to create over 20 slideshows for boxworks 2017</li>
+              <li>Worked with brand team to revamp customer pitch decks and all accompanying visuals; e.g. infographics</li>
+            </ul>
           </div>
 
           <div className="subsection">
             <span>2015&ensp;<span><a href="http://detroit.vc/" style={{"textDecoration": "none", "color": "black"}}>detroit venture partners</a>&ensp;–&ensp;graphic designer</span></span>
-            <div>Collaborated with a d.c.-based dev firm to design the new detroit.vc website</div>
-            <div>Led art direction for event campaign (250 guests) that set visuals for invitations, presentations, badges, etc</div>
+            <ul>
+              <li>Collaborated with a d.c.-based dev firm to design the new detroit.vc website</li>
+              <li>Led art direction for event campaign (250 guests) that set visuals for invitations, presentations, badges, etc</li>
+            </ul>
           </div>
 
         </div>
--- client/components/Projects.js
+++ client/components/Projects.js
@@ -12,31 +12,39 @@ export default class Projects extends React.Component {
 
           <div className="subsection">
             <span><a href="http://humansofnewyork.herokuapp.com/" style={{"textDecoration": "none", "color": "#a7a9ac"}}>humans of new york</a></span>
-            <div>Developed single page website that reinvents user experience of journalistic content</div>
-            <div>Implemented magic scroll that dynamically changes interface with user scrolling</div>
-            <div>Built collapsible nav menu from scratch while staying true to original mockup</div>
+            <ul>
+              <li>Developed single page website that reinvents user experience of journalistic content</li>
+              <li>Implemented magic scroll that dynamically changes interface with user scrolling</li>
+              <li>Built collapsible nav menu from scratch while staying true to original mockup</li>
+            </ul>
           </div>
 
           <div className="subsection">
             <span><a href="http://calcstyle.herokuapp.com/" style={{"textDecoration": "none", "color": "#a7a9ac"}}>calculator</a></span>
-            <div>Converted css to scss format which slashed code base and lead to greater readability</div>
-            <div>Implemented scss partials that modularized styling code for optimal maintainability</div>
-            <div>Demonstrated ux capabilities and thorough understanding of different logic systems</div>
+            <ul>
+              <li>Converted css to scss format which slashed code base and lead to greater readability</li>
+              <li>Implemented scss partials that modularized styling code for optimal maintainability</li>
+              <li>Demonstrated ux capabilities and thorough understanding of different logic systems</li>
+            </ul>
           </div>
 
           <div className="subsection">
             <span><a href="https://peared.io/" style={{"textDecoration": "none", "color": "#a7a9ac"}}>peared.io</a></span>
-            <div>Implemented responsive design with custom media queries for flexible ui across all devices</div>
-            <div>Built front-end views with css3 styling, applying consistent visuals across all components</div>
-            <div>Managed application state with redux for improved maintainability and data accessibility</div>
-            <div>Designed postgresql schemas for efficient querying that optimize restful apis</div>
+            <ul>
+              <li>Implemented responsive design with custom media queries for flexible ui across all devices</li>
+              <li>Built front-end views with css3 styling, applying consistent visuals across all components</li>
+              <li>Managed application state with redux for improved maintainability and data accessibility</li>
+              <li>Designed postgresql schemas for efficient querying that optimize restful apis</li>
+            </ul>
           </div>
 
           <div className="subsection">
             <span><a href="http://helpreactor.herokuapp.com/" style={{"textDecoration": "none", "color": "#a7a9ac"}}>help reactor</a></span>
-            <div>Abstracted complex data away from users by generating dynamic, interactive visual tools from recharts</div>
-            <div>Architected react hierarchy for better data flow for improved extensibility</div>
-            <div>Designed back-end routes in node to serve unique data based on user permissions</div>
+            <ul>
+              <li>Abstracted complex data away from users by generating dynamic, interactive visual tools from recharts</li>
+              <li>Architected react hierarchy for better data flow for improved extensibility</li>
+              <li>Designed back-end routes in node to serve unique data based on user permissions</li>
+            </ul>
           </div>
           
         </div>
