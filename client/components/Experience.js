7b142a793a6881baaa85c4543559a30c26fa935d
Wesley Choi <weschoi.creative@gmail.com>
2018-11-14 17:25:32 -0800

    change experience

--- client/components/Experience.js
+++ client/components/Experience.js
@@ -11,7 +11,7 @@ export default class Experience extends React.Component {
         <div className="description">
         
           <div className="subsection">
-            <span>2018&ensp;<span><a href="https://www.google.com/" style={{"textDecoration": "none", "color": "black"}}>google</a>&ensp;–&ensp;graphic designer [contract]</span></span>
+            <span>2018&ensp;<span><a href="https://www.google.com/" style={{"textDecoration": "none", "color": "black"}}>google</a>&ensp;–&ensp;graphic designer [sketch, illustrator, google slides]</span></span>
             <ul>
               <li>Implemented global rebranding of Google Ads & Google Marketing Platform alongside fellow creatives</li>
               <li>Applied newest standards to hundreds of sales collateral, now unified under a single visual brand</li>
@@ -20,7 +20,7 @@ export default class Experience extends React.Component {
           </div>
 
           <div className="subsection">
-            <span>2017&ensp;<span><a href="https://www.box.com/" style={{"textDecoration": "none", "color": "black"}}>box</a>&ensp;–&ensp;presentation designer [contract]</span></span>
+            <span>2017&ensp;<span><a href="https://www.box.com/" style={{"textDecoration": "none", "color": "black"}}>box</a>&ensp;–&ensp;presentation designer [keynote, illustrator]</span></span>
             <ul>
               <li>Applied brand guidelines and messaging strategies to create over 20 slideshows for boxworks 2017</li>
               <li>Worked with brand team to revamp customer pitch decks and all accompanying visuals; e.g. infographics</li>
@@ -28,7 +28,7 @@ export default class Experience extends React.Component {
           </div>
 
           <div className="subsection">
-            <span>2015&ensp;<span><a href="http://detroit.vc/" style={{"textDecoration": "none", "color": "black"}}>detroit venture partners</a>&ensp;–&ensp;graphic designer</span></span>
+            <span>2015&ensp;<span><a href="http://detroit.vc/" style={{"textDecoration": "none", "color": "black"}}>detroit venture partners</a>&ensp;–&ensp;graphic designer [illustrator, photoshop]</span></span>
             <ul>
               <li>Collaborated with a d.c.-based dev firm to design the new detroit.vc website</li>
               <li>Led art direction for event campaign (250 guests) that set visuals for invites, presentations, badges, etc</li>
